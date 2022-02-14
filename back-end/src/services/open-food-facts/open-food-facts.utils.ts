7ced81e48511cebb4b284853ebf07681bdfaa8dd
Vincent Bernet <vincent.bernet99@gmail.com>
2022-02-14 14:24:53 +0100

    refacto: extract function and better type definition

---
+++ back-end/src/services/open-food-facts/open-food-facts.utils.ts
@@ -0,0 +1,61 @@
+import { productFormated } from './interfaces/productResponse.interface';
+
+export const parseValuableInformation = (
+  productInformation: string,
+): productFormated => {
+  let productInformationJSON = JSON.parse(productInformation);
+
+  let productInformationFormated: productFormated = {} as productFormated;
+  productInformationFormated = productInformationJSON;
+
+  productInformationFormated.vegan = isVegan(productInformationJSON);
+  productInformationFormated.vegetarian = isVegetarian(productInformationJSON);
+  productInformationFormated.palmOilFree = isPalmOilFree(
+    productInformationJSON,
+  );
+
+  return productInformationFormated;
+};
+
+/* function to verify if the product is vegan */
+const isVegan = (productInformationJSON): boolean => {
+  if (productInformationJSON.ingredients_analysis_tags.includes('non-vegan')) {
+    return false;
+  } else if (
+    productInformationJSON.ingredients_analysis_tags.includes(
+      'vegan-status-unknown',
+    )
+  ) {
+    return undefined;
+  } else {
+    return true;
+  }
+};
+
+/* function to verify if the product is vegetarian */
+const isVegetarian = (productInformationJSON): boolean => {
+  if (
+    productInformationJSON.ingredients_analysis_tags.includes('non-vegetarian')
+  ) {
+    return false;
+  } else if (
+    productInformationJSON.ingredients_analysis_tags.includes(
+      'vegetarian-status-unknown',
+    )
+  ) {
+    return undefined;
+  } else {
+    return true;
+  }
+};
+
+/* function to verify if the product is vegetarian */
+const isPalmOilFree = (productInformationJSON): boolean => {
+  if (
+    productInformationJSON.ingredients_analysis_tags.includes('palm-oil-free')
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
