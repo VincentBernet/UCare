30f010ba3710f62d5f132c9f7b982f63b98babb8
Vincent Bernet <vincent.bernet99@gmail.com>
2022-02-16 18:49:18 +0100

    update: redirect to swagger on home page

--- back-end/src/app.service.ts
+++ back-end/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getResponse(): string {
-    return 'UCare backend is responding!';
+    return 'UCare backend is responding ! \n Please check the swagger documentation for more information on our API : https://ucare-backend.herokuapp.com/swagger';
   }
 }
