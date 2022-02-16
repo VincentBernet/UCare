7941953fc9f3ea7bf553f34280272726ea1a33cf
Vincent Bernet <vincent.bernet99@gmail.com>
2022-02-16 18:26:16 +0100

    documentation: update swagger

--- back-end/src/controllers/products/products.controller.ts
+++ back-end/src/controllers/products/products.controller.ts
@@ -1,14 +1,15 @@
 // Se limiter seulement à la définition d'endpoint au niveau des controllers, et mettre la logique au niveau des services.
 import { Controller, Get, Param } from '@nestjs/common';
 import { OpenFoodFactsService } from '../../services/open-food-facts/open-food-facts.service';
-import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
-  constructor(private openFoodFactsService: OpenFoodFactsService) { }
+  constructor(private openFoodFactsService: OpenFoodFactsService) {}
+
   @Get(':id')
-  getSpecifcProducts(@Param('id') id: Int32Array) {
+  getSpecifcProducts(@Param('id') id: number) {
     return this.openFoodFactsService.getProductInformation(id);
   }
 }
