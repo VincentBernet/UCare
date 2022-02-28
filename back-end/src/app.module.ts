a1e440a4d5ffdd3e3d37f1411f0ea7cf1036639f
Matthieu <matthieu.gedeon@efrei.net>
2022-02-28 18:55:51 +0100

    feat: login route

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -7,10 +7,11 @@ import { UsersController } from './controllers/users/users.controller';
 import { HttpModule } from 'nestjs-http-promise';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [HttpModule, AuthModule, UsersModule],
-  controllers: [AppController, ProductsController, UsersController],
+  controllers: [AppController, ProductsController, UsersController, AuthController],
   providers: [AppService, OpenFoodFactsService],
 })
 export class AppModule {}
--- back-end/src/auth/auth.module.ts
+++ back-end/src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
